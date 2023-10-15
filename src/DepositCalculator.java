77baee65c0e5048a624aa8be28c50f7b6e7b61a4
Сергей Смирнов <smirnov.dak0ta@yandex.ru>
2023-10-15 03:47:11 +0300

    Изменено имя класса и наименование файла класса; Изменены имена методов для лаконичного и логичного восприятия; Расставлены необходимые пробелы и отсупы для наилучшего восприятия; Изменены имена переменных для наилучшей читабельности кода; Соблюдена максимальная длина строки кода до 120 символов;

---
+++ src/DepositCalculator.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class DepositCalculator {
+    double calculateComplexPercent(double a, double y, int d ) {
+        double pay = a * Math.pow((1 + y / 12), 12 * d);
+
+        return roundPowedValue(pay, 2);
+    }
+
+    double calculateSimplePercent(double doubleAmount,double doubleYearRate, int depositPeriod) {
+        return roundPowedValue(doubleAmount + doubleAmount * doubleYearRate * depositPeriod, 2);
+    }
+
+    double roundPowedValue(double value, int places) {
+        double Scale = Math.pow(10, places);
+
+        return Math.round(value*Scale) / Scale;
+    }
+
+    void toInvestMoney() {
+        int period;
+        int action;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите сумму вклада в рублях:");
+        int amount = scanner.nextInt();
+
+        System.out.println("Введите срок вклада в годах:");
+        period = scanner.nextInt();
+
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        action = scanner.nextInt();
+
+        double investSum = 0;
+
+        if (action == 1) {
+            investSum = calculateSimplePercent(amount, 0.06, period);
+        } else if (action == 2) {
+            investSum = calculateComplexPercent(amount, 0.06, period);
+        }
+
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + investSum);
+    }
+
+    public static void main(String[] args) {
+        new DepositCalculator().toInvestMoney();
+    }
+}
